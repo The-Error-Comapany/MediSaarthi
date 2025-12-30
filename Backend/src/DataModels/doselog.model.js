07a9960657e3e721ab63cd65a0d69388b26cac33
kg29185048 <gargkrish152@gmail.com>
2025-12-30 13:03:12 +0530

    push doselog model

---
+++ Backend/src/DataModels/doselog.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const doseLogSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    medicationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Medication",
+      required: true,
+    },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    status: { type: String, enum: ["pending", "notified", "taken","missed"], default: "pending" },
+  },
+  { timestamps: true }
+);
+
+
+export default mongoose.model("DoseLog", doseLogSchema);
